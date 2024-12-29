e31f24bf6cf9c849e5add6086a99d24a33999583
d23c1055@Capubara <s23c1055ky@s.chibaoudai.jp>
2024-12-29 23:46:30 +0900

    test task

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,15 +6,36 @@ class Talker(Node):
     def __init__(self):
         super().__init__("talker")
         self.pub = self.create_publisher(Int16, "countup", 10)
-        self.create_timer(0.5, self.cb)
-        self.n = 0
-
+        self.create_timer(1, self.cb)
+        self.number = 2
 
     def cb(self):
-        msg = Int16()
-        msg.data = self.n
-        self.pub.publish(msg)
-        self.n += 1
+        while not self.prime(self.number):
+            self.number += 1
+        self.pub.publish(Int16(data=self.number))
+        self.number += 1
+
+    def prime(self, n):
+        if n < 2:
+            return False
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+#class Talker(Node):
+#    def __init__(self):
+#        super().__init__("talker")
+#        self.pub = self.create_publisher(Int16, "countup", 10)
+#        self.create_timer(0.5, self.cb)
+#        self.n = 0
+
+
+#    def cb(self):
+#        msg = Int16()
+#        msg.data = self.n
+#        self.pub.publish(msg)
+#        self.n += 1
 
 def main():
     rclpy.init()
