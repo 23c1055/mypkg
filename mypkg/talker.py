24998b0a85ada999e0ee847ddf3b27d7b5f8c8cf
23c1055 <s23c1055ky@s.chibakoudai.jp>
2024-12-30 09:54:33 +0900

    Update talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,29 +2,31 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+
 class Talker(Node):
     def __init__(self):
         super().__init__("talker")
         self.pub = self.create_publisher(Int16, "countup", 10)
         self.create_timer(1.0, self.cb)
-        self.number = 2
+        self.current_number = 2
 
     def cb(self):
-        while not self.is_prime(self.number):
-            self.number += 1
+        while not self.is_prime(self.current_number):
+            self.current_number += 1
         msg = Int16()
-        msg.data = self.number
+        msg.data = self.current_number
         self.pub.publish(msg)
-        self.get_logger().info(f"Sending prime: {self.number}")
-        self.number += 1
+        self.get_logger().info(f"Published prime: {msg.data}")
+        self.current_number += 1
 
-    def is_prime(self, n):
-        if n < 2:
+    def is_prime(self, number):
+        if number < 2:
             return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
                 return False
         return True
+
 
 def main():
     rclpy.init()
