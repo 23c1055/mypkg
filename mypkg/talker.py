a20fd38d1ab38224e1b73f05f069c7cd6a177902
23c1055 <s23c1055ky@s.chibakoudai.jp>
2024-12-30 18:11:37 +0900

    Update talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,17 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
 
 
 class Talker(Node):
     def __init__(self):
         super().__init__("talker")
-        self.pub = self.create_publisher(Int16, "countup", 10)
         self.create_timer(1.0, self.cb)
         self.current_number = 2
 
     def cb(self):
         while not self.is_prime(self.current_number):
             self.current_number += 1
-        msg = Int16()
-        msg.data = self.current_number
-        self.pub.publish(msg)
-        self.get_logger().info(f"Published prime: {msg.data}")
+        self.get_logger().info(f"Prime: {self.current_number}")
         self.current_number += 1
 
     def is_prime(self, number):
@@ -33,8 +28,8 @@ def main():
     node = Talker()
     try:
         rclpy.spin(node)
-    except keyboardInterrupt:
+    except KeyboardInterrupt:
         pass
     finally:
         node.destroy_node()
-    rclpy.shutdown()
+        rclpy.shutdown()
