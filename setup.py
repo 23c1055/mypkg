174412b7042c0ad9d64a5ace99d2140a983c70e4
23c1055 <s23c1055ky@s.chibakoudai.jp>
2025-01-01 01:27:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'prime = mypkg.prime:lesson10',
+            'prime = mypkg.prime:main',
         ],
     },
 )
