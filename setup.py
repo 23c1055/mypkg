90fc3e8200da473154b8c39626f396bd8beb0aa5
d23c1055@Capubara <s23c1055ky@s.chibaoudai.jp>
2025-01-02 00:57:19 +0900

    change name

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'prime = mypkg.prime:main',
+            'prime_generator = mypkg.prime_generator:main',
         ],
     },
 )
