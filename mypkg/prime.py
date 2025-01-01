36181a665634287b709eb7c981ba9c6e8f74c9a0
23c1055 <s23c1055ky@s.chibakoudai.jp>
2025-01-01 20:54:09 +0900

    Update prime.py

--- mypkg/prime.py
+++ mypkg/prime.py
@@ -1,30 +1,20 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
-import random
-
 
 class Prime(Node):
-    def __init__(self, max_number=200):
-        super().__init__(f"prime_{random.randint(0, 10000)}")
+    def __init__(self):
+        super().__init__("prime")
         self._timer = self.create_timer(1.0, self.cb)
-        self._publisher = self.create_publisher(Int32, "prime_numbers", 10)
         self._current_number = 2
-        self._max_number = max_number
-        self.get_logger().info(f"Node initialized. Finding primes up to {self._max_number}.")
 
     def cb(self):
-        if self._current_number <= self._max_number:
+        if self._current_number <= 200:
             if self.is_prime(self._current_number):
-                self.get_logger().info(f"prime: {self._current_number}")
-                msg = Int32()
-                msg.data = self._current_number
-                self._publisher.publish(msg)
+                self.get_logger().info(f"Prime: {self._current_number}")
             self._current_number += 1
         else:
-            self.get_logger().info("Completed finding primes.")
+            self.get_logger().info("Completed finding primes up to 200.")
             self._timer.cancel()
-            self.shutdown()
 
     def is_prime(self, number):
         if number < 2:
@@ -34,24 +24,17 @@ class Prime(Node):
                 return False
         return True
 
-    def shutdown(self):
-        self.get_logger().info("Shutting down node.")
-        self.destroy_node()
-        rclpy.shutdown()
-
-
 def main():
     rclpy.init()
-    max_number = 200
-    node = Prime(max_number)
+    node = Prime()
     try:
         rclpy.spin(node)
     except rclpy.executors.ExternalShutdownException:
         pass
     finally:
+        node.destroy_node()
         if rclpy.ok():
-            node.shutdown()
-
+            rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
