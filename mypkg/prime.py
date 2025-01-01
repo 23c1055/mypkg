74b8ae6b4930159d3aecf71e2ba8bed83888acf7
23c1055 <s23c1055ky@s.chibakoudai.jp>
2025-01-01 20:51:02 +0900

    Update prime.py

--- mypkg/prime.py
+++ mypkg/prime.py
@@ -1,20 +1,30 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Int32
+import random
+
 
 class Prime(Node):
-    def __init__(self):
-        super().__init__("prime")
+    def __init__(self, max_number=200):
+        super().__init__(f"prime_{random.randint(0, 10000)}")
         self._timer = self.create_timer(1.0, self.cb)
+        self._publisher = self.create_publisher(Int32, "prime_numbers", 10)
         self._current_number = 2
+        self._max_number = max_number
+        self.get_logger().info(f"Node initialized. Finding primes up to {self._max_number}.")
 
     def cb(self):
-        if self._current_number <= 200:
+        if self._current_number <= self._max_number:
             if self.is_prime(self._current_number):
-                self.get_logger().info(f"Prime: {self._current_number}")
+                self.get_logger().info(f"prime: {self._current_number}")
+                msg = Int32()
+                msg.data = self._current_number
+                self._publisher.publish(msg)
             self._current_number += 1
         else:
-            self.get_logger().info("Completed finding primes up to 200.")
+            self.get_logger().info("Completed finding primes.")
             self._timer.cancel()
+            self.shutdown()
 
     def is_prime(self, number):
         if number < 2:
@@ -24,17 +34,24 @@ class Prime(Node):
                 return False
         return True
 
+    def shutdown(self):
+        self.get_logger().info("Shutting down node.")
+        self.destroy_node()
+        rclpy.shutdown()
+
+
 def main():
     rclpy.init()
-    node = Prime()
+    max_number = 200
+    node = Prime(max_number)
     try:
         rclpy.spin(node)
     except rclpy.executors.ExternalShutdownException:
         pass
     finally:
-        node.destroy_node()
         if rclpy.ok():
-            rclpy.shutdown()
+            node.shutdown()
+
 
 if __name__ == "__main__":
     main()
