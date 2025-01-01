05a7930e6113cfcd722a92d095f99347f189c43d
23c1055 <s23c1055ky@s.chibakoudai.jp>
2025-01-01 16:46:51 +0900

    Update prime.py

--- mypkg/prime.py
+++ mypkg/prime.py
@@ -5,17 +5,17 @@ from rclpy.node import Node
 class Prime(Node):
     def __init__(self):
         super().__init__("prime")
-        self.create_timer(1.0, self.cb)
-        self.current_number = 2
+        self._timer = self.create_timer(1.0, self.cb)
+        self._current_number = 2
 
     def cb(self):
-        if self.current_number <= 200:
-            if self.is_prime(self.current_number):
-                self.get_logger().info(f"Prime: {self.current_number}")
-            self.current_number += 1
+        if self._current_number <= 200:
+            if self.is_prime(self._current_number):
+                self.get_logger().info(f"Prime: {self._current_number}")
+            self._current_number += 1
         else:
             self.get_logger().info("Completed finding primes up to 200.")
-            self.get_clock().cancel_timer(self.timer)
+            self._timer.cancel()  # タイマーを停止
 
     def is_prime(self, number):
         if number < 2:
@@ -37,6 +37,7 @@ def main():
         node.destroy_node()
         if rclpy.ok():
             rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
