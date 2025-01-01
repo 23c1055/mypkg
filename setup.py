accef4008227ae0dd94b5b3a95cae3ee43da12f6
23c1055 <s23c1055ky@s.chibakoudai.jp>
2025-01-01 21:01:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from glob import glob
 
 package_name = 'mypkg'
 
-# launch ファイルの存在確認
-launch_files = glob('launch/*.launch.py') if os.path.exists('launch') else []
-
 setup(
     name=package_name,
     version='0.0.0',
@@ -15,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), launch_files)
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
